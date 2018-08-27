38a3984ea4d6dcddc7ee13316520fe46005306a0
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-08-27 18:23:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let customerName = "bob";
+
+function upperCaseCustomerName(arg){
+  return arg.
+}
+upperCaseCustomerName()
+modifies the customerName variable ‣
+AssertionError: expected undefined to equal 'bob'
+    at Context.<anonymous> (test/indexTest.js:12:38)
+setBestCustomer()
+setBestCustomer ‣
+ReferenceError: setBestCustomer is not defined
+    at Context.<anonymous> (test/indexTest.js:23:7)
+overwriteBestCustomer()
+overwrites the best customer ‣
+ReferenceError: overwriteBestCustomer is not defined
+    at Context.<anonymous> (test/indexTest.js:31:7)
+changeLeastFavoriteCustomer()
+unsuccessfully tries to reassign the least favorite customer ‣
+ReferenceError: changeLeastFavoriteCustomer is not defined
+    at Context.<anonymous> (test/indexTest.js:39:14)
