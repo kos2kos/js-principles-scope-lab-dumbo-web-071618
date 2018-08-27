418612096dafcc43bb6ade9a5e5b2e00e317fedd
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-08-27 18:40:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// Write your solution in this file!
 let customerName = 'bob';
 const leastFavoriteCustomer = "YOUR MOM";
 function upperCaseCustomerName(arg){
   return arg.toUpperCase()
-}
+};
 
 function setBestCustomer() {
   var bestCustomer = 'not bob';
-}
+};
 
 function overwriteBestCustomer(){
   var bestCustomer = 'maybe bob';
-}
+};
 
 function changeLeastFavoriteCustomer(){
   LeastFavoriteCustomer = arg;
-}
+};
 
 
